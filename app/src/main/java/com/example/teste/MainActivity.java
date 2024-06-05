6b86102bf6b0f12481333e3a8678c949a846e6fd
RENATO <renato.mori2005@gmail.com>
2024-06-05 10:41:56 -0300

    Primeir

--- app/src/main/java/com/example/teste/MainActivity.java
+++ app/src/main/java/com/example/teste/MainActivity.java
@@ -73,5 +73,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+
+
+        //apagar
     }
 }
